39abac2b64e4119ecd6f91dc6c3c269a0f930c05
Cecil Han <cecil.hanjj@gmail.com>
2023-06-30 13:39:57 +0800

    Install psycopg2 into image

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator funtions
+"""
+
+
+def add(x, y):
+    """Add x and y and return result."""
+    return x + y
+
+def subtract(x, y):
+    return y - x
--- app/app/settings.py
+++ app/app/settings.py
@@ -80,6 +80,16 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'HOST': os.environ.get('DB_HOST'),
+#         'NAME': os.environ.get('DB_NAME'),
+#         'USER': os.environ.get('DB_USER'),
+#         'PASSWORD': os.environ.get('DB_PASS'), 
+#     }
+# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
